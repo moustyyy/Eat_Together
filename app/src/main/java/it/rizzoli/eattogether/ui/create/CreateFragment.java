31b0fbb80c6a72e9b937c3c7ac0a00607ef74537
Andreasdds <acioffibest@gmail.com>
2024-12-18 17:19:03 +0100

    fix fix

--- app/src/main/java/it/rizzoli/eattogether/ui/create/CreateFragment.java
+++ app/src/main/java/it/rizzoli/eattogether/ui/create/CreateFragment.java
@@ -1,14 +1,21 @@
 package it.rizzoli.eattogether.ui.create;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import it.rizzoli.eattogether.R;
+import it.rizzoli.eattogether.activity.MainActivity;
 
 
 public class CreateFragment extends Fragment {
@@ -16,7 +23,27 @@ public class CreateFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_create, container, false);
+
+        View root = inflater.inflate(R.layout.fragment_create, container, false);
+
+        ImageView imageView = root.findViewById(R.id.imageView);
+        Button selectImageButton = root.findViewById(R.id.uploadImage);
+
+        var pickImage = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri result) {
+                if (result != null) {
+                    imageView.setImageURI(result);
+                    imageView.setVisibility(View.VISIBLE);
+                    selectImageButton.setVisibility(View.GONE);
+
+                }
+            }
+        });
+
+        selectImageButton.setOnClickListener(v -> pickImage.launch("image/*"));
+
+        return root;
     }
 
     @Override
