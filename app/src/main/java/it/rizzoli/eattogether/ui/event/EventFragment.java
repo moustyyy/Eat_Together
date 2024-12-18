bc37b4ebc13f087b6e60fa416c71a7da328889bc
Andreasdds <acioffibest@gmail.com>
2024-12-18 21:04:45 +0100

    fix fix

--- app/src/main/java/it/rizzoli/eattogether/ui/event/EventFragment.java
+++ app/src/main/java/it/rizzoli/eattogether/ui/event/EventFragment.java
@@ -1,6 +1,8 @@
 package it.rizzoli.eattogether.ui.event;
 
 import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -27,11 +29,11 @@ import it.rizzoli.eattogether.ui.food_boox.FragmentFoodBox;
 
 public class EventFragment extends Fragment implements FoodBoxAdapter.FragmentTransactionListener {
     private ImageView eventImage;
-    private TextView eventName, eventDate, eventUserCreator;
+    private TextView eventName, eventDate, eventOrganizer, eventTime, eventDescription, eventAddress;
     private DatabaseHelper databaseHelper;
     private ListView foodBoxListView;
 
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint({"MissingInflatedId", "Range"})
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -40,7 +42,10 @@ public class EventFragment extends Fragment implements FoodBoxAdapter.FragmentTr
         eventImage = root.findViewById(R.id.eventImage);
         eventName = root.findViewById(R.id.eventName);
         eventDate = root.findViewById(R.id.eventDate);
-        eventUserCreator = root.findViewById(R.id.eventUserCreator);
+        eventTime = root.findViewById(R.id.eventTime);
+        eventOrganizer = root.findViewById(R.id.eventOrganizer);
+        eventDescription = root.findViewById(R.id.eventDescription);
+        eventAddress = root.findViewById(R.id.eventAddress);
 
         databaseHelper = new DatabaseHelper(getContext());
         foodBoxListView = root.findViewById(R.id.foodBoxList);
@@ -64,10 +69,24 @@ public class EventFragment extends Fragment implements FoodBoxAdapter.FragmentTr
             }
 
             Event event = databaseHelper.getEventById(eventId);
+            String username = "";
+            SQLiteDatabase db = databaseHelper.getWritableDatabase();
+            String sql = "SELECT * FROM Users WHERE _id = ?";
+            String[] selectionArgs = new String[] { String.valueOf(event.getIdUserCreator()) };
+            Cursor cursor = db.rawQuery(sql, selectionArgs);
+
+            if (cursor.moveToFirst()) {
+                username = cursor.getString(cursor.getColumnIndex("username"));
+            }
+
+            cursor.close();
             if (event != null) {
                 eventName.setText(event.getNome());
                 eventDate.setText(event.getData());
-                eventUserCreator.setText(String.valueOf(event.getIdUserCreator()));
+                eventOrganizer.setText(username);
+                eventTime.setText(event.getOra());
+                eventDescription.setText(event.getDescrizione());
+                eventAddress.setText(event.getIndirizzo() + ", " + event.getCitta());
             }
         }
 
