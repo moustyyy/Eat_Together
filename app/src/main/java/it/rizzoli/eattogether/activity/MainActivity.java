54915c872c6186e20d7b417053c2a90a5383b2ea
moustyyy <160880804+moustyyy@users.noreply.github.com>
2024-12-18 13:02:48 +0100

    add in home a random event names

--- app/src/main/java/it/rizzoli/eattogether/activity/MainActivity.java
+++ app/src/main/java/it/rizzoli/eattogether/activity/MainActivity.java
@@ -9,15 +9,24 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 import it.rizzoli.eattogether.R;
+import it.rizzoli.eattogether.adapter.EventAdapter;
 import it.rizzoli.eattogether.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private RecyclerView recyclerView;
+    private EventAdapter eventAdapter;
+    private List<String> eventNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +39,24 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home,R.id.navigation_create, R.id.navigation_profile)
+                R.id.navigation_home, R.id.navigation_create, R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-    }
 
+        recyclerView = findViewById(R.id.recycler_view);
+
+        eventNames = new ArrayList<>(Arrays.asList(
+                "Festa di Natale",
+                "Capodanno",
+                "Evento Benefico"
+        ));
+
+        eventAdapter = new EventAdapter(eventNames);
+        recyclerView.setAdapter(eventAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
 }
+
+
---
+++ app/src/main/java/it/rizzoli/eattogether/adapter/EventAdapter.java
@@ -0,0 +1,53 @@
+package it.rizzoli.eattogether.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import it.rizzoli.eattogether.R;
+
+public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHolder> {
+
+    private List<String> eventNames;  // Lista dei nomi degli eventi
+
+    public EventAdapter(List<String> eventNames) {
+        this.eventNames = eventNames;
+    }
+
+    @NonNull
+    @Override
+    public EventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_event_box, parent, false);
+        return new EventViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull EventViewHolder holder, int position) {
+        // Imposta il nome dell'evento nella TextView
+        String eventName = eventNames.get(position);
+        holder.eventNameTextView.setText(eventName);
+    }
+
+    @Override
+    public int getItemCount() {
+        return eventNames.size();
+    }
+
+    public static class EventViewHolder extends RecyclerView.ViewHolder {
+        TextView eventNameTextView;
+
+        public EventViewHolder(@NonNull View itemView) {
+            super(itemView);
+            eventNameTextView = itemView.findViewById(R.id.event_name);
+        }
+    }
+}
+
